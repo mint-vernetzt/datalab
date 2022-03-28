be964fdb60a111a06a35dc8cb44452dae8c76f28
simonhassler-stat <simon.hassler@statworx.com>
2022-03-28 14:34:42 +0200

    exchange choices in submodules

--- R/mod_studium_abschluss_choice_2.R
+++ R/mod_studium_abschluss_choice_2.R
@@ -10,42 +10,55 @@
 mod_studium_abschluss_choice_2_ui <- function(id){
   ns <- NS(id)
   tagList(
-
     shiny::column(
       width = 6,
-      shinyWidgets::prettyRadioButtons(
-        inputId = ns("date_abschluss"),
-        label = "Wähle ein Jahr:",
-        choices = c(2012, 2015, 2018, 2020),
-        selected = 2012,
-        inline = TRUE,
-        fill = TRUE
+      shinyWidgets::sliderTextInput(
+        inputId = ns("date_abschluss_1"),
+        label = "Wähle ein Zeitraum:",
+        choices = c(2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020),
+        selected = c(2012, 2020),
+        from_min = 2012,
+        from_max = 2014,
+        to_min = 2015,
+        to_max = 2020
       ),
       shinyWidgets::radioGroupButtons(
-        inputId = ns("geschlecht_abschluss"),
+        inputId = ns("geschlecht_abschluss_1"),
         label = "Soll die Aufteilung nach Geschlecht getrennt werden ?",
         choices = c("Ja", "Nein"),
         justified = TRUE,
         checkIcon = list(yes = icon("ok",
                                     lib = "glyphicon"))
+      ),
+      shinyWidgets::awesomeCheckboxGroup(
+        inputId = ns("indikator_abschluss_1"),
+        label = "Indikator",
+        choices = c("bachelor", "fh", "lehramt", "master", "promotion", "uni"),
+        selected = "bachelor"
       )
+
     )
+
   )
 }
 
 #' studium_abschluss_choice_2 Server Functions
 #'
 #' @noRd
-mod_studium_abschluss_choice_2_server <- function(id,r){
+mod_studium_abschluss_choice_2_server <- function(id, r){
   moduleServer( id, function(input, output, session){
     #ns <- session$ns
 
-    observeEvent(input$geschlecht_abschluss, {
-      r$geschlecht_abschluss <- input$geschlecht_abschluss
+    observeEvent(input$geschlecht_abschluss_1, {
+      r$geschlecht_abschluss_1 <- input$geschlecht_abschluss_1
     })
 
-    observeEvent(input$date_abschluss, {
-      r$date_abschluss <- input$date_abschluss
+    observeEvent(input$date_abschluss_1, {
+      r$date_abschluss_1 <- input$date_abschluss_1
+    })
+
+    observeEvent(input$indikator_abschluss_1, {
+      r$indikator_abschluss_1 <- input$indikator_abschluss_1
     })
 
   })
