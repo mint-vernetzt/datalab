df3bf6b71d2dd8e9f7c649096fe8ab3a2e91362a
simonhassler-stat <simon.hassler@statworx.com>
2022-03-28 14:10:45 +0200

    add dictionary for titles

---
+++ R/utils_helpers.R
@@ -0,0 +1,11 @@
+#' helpers
+#'
+#' @description A utils function
+#'
+#' @return The return value, if any, from executing the utility.
+#'
+#' @noRd
+
+dictionary_title_studium_studentenzahl <- list("eingeschrieben" = "die insgesamt eingeschrieben sind",
+                                               "1hs" = "die im 1. Hochschulsemester eingeschrieben sind",
+                                               "1fs" = "die im 1. Fachsemester eingeschrieben sind")
