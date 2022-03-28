8ddb3ca241a7dea9fe0c4352969d4f9a6d6264a4
simonhassler-stat <simon.hassler@statworx.com>
2022-03-28 14:31:24 +0200

    add modules to main page of studium

--- R/mod_studium.R
+++ R/mod_studium.R
@@ -10,6 +10,26 @@
 mod_studium_ui <- function(id){
   ns <- NS(id)
   tagList(
+    fluidPage(
+      fluidRow(
+        shinydashboard::box(
+          title = "Studentenzahl nach Fach",
+          width = 12,
+          shiny::mainPanel(
+            mod_studium_studienzahl_ui("mod_studium_studienzahl_ui_1")
+          )
+        )
+      ),
+      fluidRow(
+        shinydashboard::box(
+          title = "Abschlusszahlen nach akademischem Grad",
+          width = 12,
+          shiny::mainPanel(
+            mod_studium_abschluss_ui("mod_studium_abschluss_ui_1")
+          )
+        )
+      )
+    )
 
   )
 }
@@ -17,11 +37,29 @@ mod_studium_ui <- function(id){
 #' studium Server Functions
 #'
 #' @noRd
-mod_studium_server <- function(id){
-  moduleServer( id, function(input, output, session){
-    ns <- session$ns
+mod_studium_server <- function(id, data, r){
 
-  })
+  r <- reactiveValues(geschlecht = NULL,
+                      date = NULL,
+                      indikator = NULL,
+                      geschlecht_waffle = NULL,
+                      date_waffle = NULL,
+                      indikator_waffle = NULL,
+                      date_abschluss = NULL,
+                      geschlecht_abschluss = NULL,
+                      date_abschluss_1 = NULL,
+                      geschlecht_abschluss_1 = NULL,
+                      indikator_abschluss_1 = NULL)
+  # comment
+  mod_studium_studienzahl_choice_1_server("mod_studium_studienzahl_choice_ui_1_1", r)
+  mod_studium_studienzahl_choice_2_server("mod_studium_studienzahl_choice_ui_2_1", r)
+  mod_studium_studienzahl_server("mod_studium_studienzahl_ui_1", data, r)
+
+  # comment
+  mod_studium_abschluss_choice_1_server("mod_studium_abschluss_choice_ui_1_1", r)
+  mod_studium_abschluss_choice_2_server("mod_studium_abschluss_choice_ui_2_1", r)
+  mod_studium_abschluss_server("mod_studium_abschluss_ui_1", data, r)
+
 }
 
 ## To be copied in the UI
