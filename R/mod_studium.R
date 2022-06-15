d7a138834a43192d12f91e0861bb1d6366de48db
Andreas Vogl <andreas.vogl@statworx.com>
2022-06-15 20:22:32 +0200

    clean up Studium

--- R/mod_studium.R
+++ R/mod_studium.R
@@ -23,83 +23,37 @@ mod_studium_ui <- function(id){
 #' @noRd
 mod_studium_server <- function(id, data_studierende, r){
 
-  r <- reactiveValues(date_studierende_einstieg = NULL,
-                      nurLehramt_studierende_einstieg = NULL,
-                      hochschulform_studierende_einstieg_1 = NULL,
-                      hochschulform_studierende_einstieg_2 = NULL,
-
-                      date_studierende_einstieg_gender = NULL,
-                      nurLehramt_studierende_einstieg_gender = NULL,
-                      hochschulform_studierende_einstieg_1_gender = NULL,
-                      hochschulform_studierende_einstieg_2_gender = NULL,
-
-                      date_studienzahl_einstieg_verlauf = NULL,
-                      nurLehramt_studierende_einstieg_verlauf = NULL,
-                      hochschulform_studierende_einstieg_verlauf_1 = NULL,
-                      hochschulform_studierende_einstieg_verlauf_2 = NULL,
-
-                      date_studienzahl_einstieg_verlauf_gender = NULL,
-                      nurLehramt_studierende_einstieg_verlauf_gender = NULL,
-                      hochschulform_studierende_einstieg_verlauf_gender_1 = NULL,
-                      hochschulform_studierende_einstieg_verlauf_gender_2 = NULL,
-
-                      date_kurse_einstieg_comparison = NULL,
-                      date_kurse_einstieg_comparison_gender = NULL,
-
-
-                       gender_switch = NULL,
-                       date_studierende = NULL,
-                       nurLehramt_studierende = NULL,
-                       hochschulform_studierende_1 = NULL,
-                       hochschulform_studierende_2 = NULL,
-                       date_studierende_verlauf = NULL,
-                       indikator_studierende_verlauf = NULL,
-                       topic_studierende_verlauf = NULL,
-                       nurLehramt_studierende_verlauf = NULL,
-                       states_studierende_verlauf = NULL,
-                       date_studierende_verlauf_bl = NULL,
-                       topic_studierende_verlauf_bl = NULL,
-                       nurLehramt_studierende_verlauf_bl = NULL,
-                       states_studierende_verlauf_bl = NULL,
-                       date_verlauf_subject_bl = NULL,
-                       topic_selected_subject_bl = NULL,
-                       subject_selected_bl = NULL,
-                       states_verlauf_subject_bl = NULL,
-                       nurLehramt_studierende_verlauf_bl_subject = NULL,
-                       subjects_aggregated = NULL,
-                       subject_selected = NULL,
-                       subjects_aggregated_bl = NULL,
-                       subject_selected_bl = NULL
-                     )
-
-
+  r <- reactiveValues()
 
   # Studienzahl
-  mod_studium_studienzahl_choice_1_server("mod_studium_studienzahl_choice_ui_1_1", r)
+  mod_studium_studienzahl_server("mod_studium_studienzahl_ui_1", data_studierende, r)
 
+  # Box 2
   mod_studium_studienzahl_einstieg_server("mod_studium_studienzahl_einstieg_ui_1", r)
-  mod_studium_studienzahl_einstieg_gender_server("mod_studium_studienzahl_einstieg_gender_ui_1", r)
-
   mod_studium_studienzahl_einstieg_verlauf_server("mod_studium_studienzahl_einstieg_verlauf_ui_1", r)
-  mod_studium_studienzahl_einstieg_verlauf_gender_server("mod_studium_studienzahl_einstieg_verlauf_gender_ui_1", r)
-
   mod_studium_studienzahl_einstieg_comparison_server("mod_studium_studienzahl_einstieg_comparison_ui_1", r)
+
+  # Box 3
+  mod_studium_studienzahl_einstieg_gender_server("mod_studium_studienzahl_einstieg_gender_ui_1", r)
+  mod_studium_studienzahl_einstieg_verlauf_gender_server("mod_studium_studienzahl_einstieg_verlauf_gender_ui_1", r)
   mod_studium_studienzahl_einstieg_comparison_gender_server("mod_studium_studienzahl_einstieg_comparison_gender_ui_1", r)
 
-  mod_studium_studienzahl_verlauf_server("mod_studium_studienzahl_verlauf_ui_1", r)
-  mod_studium_studienzahl_verlauf_bl_server("mod_studium_studienzahl_verlauf_bl_ui_1", r)
+  # Box 4
+  mod_studium_studienzahl_choice_1_server("mod_studium_studienzahl_choice_ui_1_1", r)
   mod_studium_studienzahl_verlauf_bl_subject_server("mod_studium_studienzahl_verlauf_bl_subject_ui_1", r)
-  mod_studium_studienzahl_server("mod_studium_studienzahl_ui_1", data_studierende, r)
   mod_studium_studienzahl_ranking_bl_subject_server("mod_studium_studienzahl_ranking_bl_subject_ui_1", r)
 
+  # Box 5
   mod_studium_choice_gender_server("mod_studium_studienzahl_choice_gender_ui",r)
   mod_studium_studienzahl_verlauf_bl_subject_gender_server("mod_studium_studienzahl_verlauf_bl_subject_gender_ui_1",r)
   mod_studium_studienzahl_ranking_bl_subject_gender_server("mod_studium_studienzahl_ranking_bl_subject_gender_ui_1", r)
 
+  # Box 6
   mod_studium_studienzahl_bl_map_server("mod_studium_studienzahl_bl_map", r)
   mod_studium_studienzahl_bl_verlauf_server("mod_studium_studienzahl_bl_verlauf", r)
   mod_studium_studienzahl_bl_vergleich_server("studium_studienzahl_bl_vergleich", r)
 
+  # Box 7
   mod_studium_studienzahl_bl_map_gender_server("mod_studium_studienzahl_bl_map_gender", r)
   mod_studium_studienzahl_bl_verlauf_gender_server("mod_studium_studienzahl_bl_verlauf_gender", r)
   mod_studium_studienzahl_bl_vergleich_gender_server("mod_studium_studienzahl_bl_vergleich_gender_ui", r)
