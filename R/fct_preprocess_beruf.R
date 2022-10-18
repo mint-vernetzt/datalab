b529d4bb7680b895c6463f1c1d600cd883eff8c6
Brunner <Katharina.Brunner@stifterverband.de>
2022-10-18 15:53:48 +0200

    Ergänzung Korrektur Beruf Box 2

--- R/fct_preprocess_beruf.R
+++ R/fct_preprocess_beruf.R
@@ -147,6 +147,9 @@ calc_arbeitsmarkt_share_bl <- function(df) {
     dplyr::mutate(wert = wert[fachbereich == "Alle"]) %>%
     dplyr::select(-fachbereich)
 
+  #löschen doppelter Zeilen, die durch Ersetzten vs. falsch Aufsummieren entstanden sind
+  df_alle <- unique.data.frame(df_alle)
+
   df_employed <- df %>% dplyr::filter(indikator == "Beschäftigte") %>%
     dplyr::rename(wert_employed = wert)
 
@@ -168,6 +171,7 @@ calc_arbeitsmarkt_share_bl <- function(df) {
     dplyr::select(-wert_trainee)
 
   df_return <- rbind(df_employed, df_trainee)
+
 
   return(df_return)
 }
