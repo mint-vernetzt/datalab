e61e7a7771ae489269b4274e2fcfad6e78d05a69
Brunner <Katharina.Brunner@stifterverband.de>
2022-10-21 15:32:28 +0200

    Betaversion in Banner ergänzt

--- R/mod_startseite_ui.R
+++ R/mod_startseite_ui.R
@@ -34,7 +34,7 @@ mod_startseite_ui <- function(id){
     fluidRow(
       shinydashboard::box(
         width = 12,
-        img(src='www/Banner_Willkommen_ohne_Logo.jpg',
+        img(src='www/Banner_Willkommen_beta_rot.jpg',
             class = "img-responsive",
             height = "300px",
             # width = "150px",
