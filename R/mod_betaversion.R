393ec537c6c100a6ac27c19e0e0054d3b65612e9
Kröger <Antonia.Kroeger@stifterverband.de>
2022-10-27 13:31:56 +0200

    Testanpassungen

--- R/mod_betaversion.R
+++ R/mod_betaversion.R
@@ -45,34 +45,47 @@ mod_betaversion_ui <- function(id){
     fluidRow(
         shinydashboard::box(
           width = 9,
-          title = "Betaversion",
+          title = "Wo wir stehen: Betaversion",
           p(style = "text-align: justify; font-size = 16px",
-            "Das Projekt MINT-DataLab ist 2021 gestartet. Unser Ziel ist es, bis 2025 ein umfassendes MINT-DataLab aufzubauen.
+            "Das Projekt MINT-DataLab ist 2021 gestartet. Unser Ziel ist es, bis 2025 mit den uns zur Verfügungen stehenden Ressourcen ein umfassendes MINT-DataLab aufzubauen.
             Dies soll bestehende amtliche Statistiken zu MINT und Studienergebnisse aus den Bereichen Frühkindliche Bildung, Schulbildung, außerschulische Bildung,
             Hochschulbildung, berufliche Ausbildung, Weiterbildung sowie Arbeitsmarkt umfassen.",
             br(), br(),
             "Bei MINTvernetzt erheben wir selbst Daten zum Bereich außerschulische Bildung, die wir
             perspektivisch hier einbinden werden."
-          ))),
+
+          ),
+          p(style = "text-align: justify; font-size = 16px",
+            "Das MINT-DataLab ist ein Angebot aus dem Projekt ", tags$a(href="https://mint-vernetzt.de/", "MINTvernetzt", target="_blank"), "."
+          )),
 
     fluidRow(
       shinydashboard::box(
         width = 12,
-        title = "What's coming next: Welche neuen Datensätze und Services sind gerade in Pipeline?",
+        title = "Was wir vorhaben: Welche neuen Datensätze und Darstellungen sind gerade in Pipeline?",
         p(style = "text-align: justify; font-size = 16px",
+
+          "Neue Datensätze",
+          tags$li("Regionale Daten: Bisher zeigen wir Daten auf Bundes- und Landesebene. Unser Ziel ist es, auch Vergleiche bzw. Aufwertungen auf regionaler Ebene zu ermöglichen."),
+          tags$li("Mehr Bereiche abdecken: Frühkindliche Bidlung, außerschulsche Bildung und Weiterbildung fehlen noch im MINT-DataLab.
+          Je nach Datenverfügbarkeit bemühen wir uns diese Bereiche ebenfalls hier zu beleuchten. Auch allgemeine demografische und sozioökonomische Kennzahlen werden wir aufnehmen."),
+          tags$li("Mehr inhaltliche Dimensionen: Bisher zeigen wir nur die Anteile von MINT und den Anteil Frauen in MINT. Besonders für den Schulbereich werden wir noch Kompetenzdaten ergänzen.
+                  Außerdem wollen wir unsere Auswertungen um ausländsiche Studierende, Auszubildende und Fachkräfte erweitern."),
+          tags$li("Fokus auf Fachkräfte: Die Kennzahlen um Beschäftige werden wir noch um differenziertere Kennzahlen zu Fachkräften erweitern."),
+
+          "Neue Darstellungen", br(),
           tags$li("Absolute Zahlen: Bisher zeigen wir nur Prozentangaben. In der nächsten Entwicklungsphase werden wir
-                  auch die absoluten Zahlen integrieren."), br(),
-          tags$li("Download-Option für Diagramme: Unser Ziel ist es, dass unsere Diagramme bestmöglich weiterverwendet werden können. Deshalb werden wir bald eine Download-Option ergänzen."), br(),
-          tags$li("Barrierefreiheit der Grafiken"), br(),
-          tags$li("Regionale Daten: Bisher zeigen wir Daten auf Bundes- und Landesebene. Unser Ziel ist es, auch Vergleiche bzw. Aufwertungen auf regionaler Ebene zu ermöglichen.")
-        ), br(), br(),
+                  auch die absoluten Zahlen integrieren."),
+          tags$li("Download-Option für Diagramme: Unser Ziel ist es, dass unsere Diagramme bestmöglich weiterverwendet werden können. Deshalb werden wir bald eine Download-Option ergänzen."),
+          tags$li("Barrierefreiheit der Grafiken: Unsere Grafiken sind leider noch nicht barrierefrei. Hier bemühen wir uns um Verbesserungen!"),
+          br(), br(),
 
         p(style = "text-align: justify; font-size = 16px",
           span("Wir freuen uns immer über Rückfragen oder Feedback ", tags$a(href = "mailto:antonia.kroeger@mint-vernetzt.de?subject= Feedback MINT-Datalab", "per Email"),"oder über unsere kurze",
                tags$a(href="https://survey.lamapoll.de/MINT-DataLab_Feedback/", "Umfrage", target="_blank"), "!"
           )
         )
-      )),
+      ))),
 
 
 
