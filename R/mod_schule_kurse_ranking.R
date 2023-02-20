c4abdf08639b1dca37cf7f5499cee49109439f66
Kröger <Antonia.Kroeger@stifterverband.de>
2023-02-20 16:17:36 +0100

    Neues Feld unter Auswahl: Lesehilft

--- R/mod_schule_kurse_ranking.R
+++ R/mod_schule_kurse_ranking.R
@@ -40,8 +40,10 @@ mod_schule_kurse_ranking_ui <- function(id){
                   "Westen",
                   "Osten"
                   ),
-       selected = "Hessen"
-    )
+       selected = "Mecklenburg-Vorpommern"
+    ),
+br(), br(),
+    p("Lesehilfe: Hier zeigen wir einen direkten Vergleich zwischen den Anteilen von Mädchen in Grundkursen und Leistungskursen. Die Grafik kann wie ein Balkendigramm gelesen werden. Die Verbindungslinie zwischen den Punkten verdeutlicht lediglich die Diskrepanz zwischen den Werten.")
   )
 }
 
