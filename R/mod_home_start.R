8468959ffdac674d36d0f331aadb20c827c77f07
Kröger <Antonia.Kroeger@stifterverband.de>
2023-02-28 13:38:17 +0100

    Copyright 2023

--- R/mod_home_start.R
+++ R/mod_home_start.R
@@ -187,7 +187,7 @@ tags$footer(style="text-align: justify;background-color:white",
                 p(tags$a("Impressum", href="#shiny-tab-impressum", "data-toggle" = "tab")," | ",
                   tags$a("Kontakt", href="#shiny-tab-kontakt", "data-toggle" = "tab")," | ",
                   tags$a("Datenschutz", href="#shiny-tab-datenschutz", "data-toggle"="tab"),HTML('&nbsp;'),HTML('&nbsp;'),
-                  "Copyright © 2022. Alle Rechte vorbehalten Stifterverband")),
+                  "Copyright © 2023. Alle Rechte vorbehalten Stifterverband")),
 
             div(style="display: inline-block;position: relative;padding: 1em;",
 
