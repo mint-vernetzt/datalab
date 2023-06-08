a9185615397c176f9b22a429b099c8db6489b4b8
Brunner <Katharina.Brunner@stifterverband.de>
2023-06-08 14:31:32 +0200

    conditional panel fächer nach lehramt

--- R/mod_studium_studienzahl_bl_map.R
+++ R/mod_studium_studienzahl_bl_map.R
@@ -34,45 +34,85 @@ mod_studium_studienzahl_bl_map_ui <- function(id){
         "max-options" = 2,
         "max-options-text" = "Maximal 2 Indikatoren auswählen")
     ),
+    #Conditional Panel, um für Lehramt nur sinnvollere Fächer auswählen zu lassen
     p("Fächer/Fächergruppen (max. 2):"),
-    shinyWidgets::pickerInput(
-      inputId = ns("map_f"),
-      choices = c("Agrar-, Forst- und Ernährungswissenschaften, Veterinärmedizin",
-                  "Biologie",
-                  "Elektrotechnik und Informationstechnik",
-                  "Geowissenschaften und Geographie",
-                  "Informatik",
-                  "Ingenieurwissenschaften ohne Informatik",
-                  "Maschinenbau/Verfahrenstechnik",
-                  "Mathematik",
-                  "MINT",
-                  "Nicht MINT",
-                  "Physik, Astronomie",
-                  "Rechts-, Wirtschafts- und Sozialwissenschaften",
-                  "Vermessungswesen",
-                  "Architektur, Innenarchitektur",
-                  "Bauingenieurwesen",
-                  "Chemie",
-                  "Geisteswissenschaften",
-                  "Humanmedizin/Gesundheitswissenschaften",
-                  "Ingenieurwissenschaften",
-                  "Kunst, Kunstwissenschaft",
-                  "Materialwissenschaft und Werkstofftechnik",
-                  "Mathematik, Naturwissenschaften",
-                  "Naturwissenschaften",
-                  "Pharmazie",
-                  "Sport",
-                  "Verkehrstechnik, Nautik",
-                  "Wirtschaftsingenieurwesen mit ingenieurwissenschaftlichem Schwerpunkt"
-      ),
-      selected = c(
-                   "Informatik", "MINT"),
-      multiple = TRUE,
-      options =  list(
-        "max-options" = 2,
-        "max-options-text" = "Maximal 2 Indikatoren auswählen")
-    )
+    conditionalPanel(condition = "input.map_l == 'Studierende (Nur Lehramt)'",
+                     ns = ns,
+                     shinyWidgets::pickerInput(
+                       inputId = ns("map_f_lehr"),
+                       choices = c("Agrar-, Forst- und Ernährungswissenschaften, Veterinärmedizin",
+                                   "Biologie",
+                                   "Geowissenschaften und Geographie",
+                                   "Informatik",
+                                   "Maschinenbau/Verfahrenstechnik",
+                                   "Nicht MINT",
+                                   "MINT",
+                                   "Chemie",
+                                   "Mathematik",
+                                   "Humanmedizin/Gesundheitswissenschaften",
+                                   "Geisteswissenschaften",
+                                   "Ingenieurwissenschaften",
+                                   "Ingenieurwissenschaften ohne Informatik",
+                                   "Physik, Astronomie",
+                                   "Rechts-, Wirtschafts- und Sozialwissenschaften",
+                                   "Mathematik, Naturwissenschaften",
+                                   "Naturwissenschaften",
+                                   "Sport",
+                                   "Kunst, Kunstwissenschaft"),
 
+                       selected = c(
+                         "Informatik", "MINT"),
+                       multiple = TRUE,
+                       options =  list(
+                         "max-options" = 2,
+                         "max-options-text" = "Maximal 2 Indikatoren auswählen")
+                     )),
+
+    conditionalPanel(condition = "input.map_l == 'Auländische Studienanfänger:innen (1. Hochschulsemester)' |
+                     input.map_l == 'Studienanfänger:innen (1. Fachsemester)' |
+                     input.map_l == 'Studierende' |
+                     input.map_l == 'Ausländische Studierende' |
+                     input.map_l == 'Studienanfänger:innen (1. Hochschulsemester)'",
+                     ns = ns,
+                     shinyWidgets::pickerInput(
+                       inputId = ns("map_f"),
+
+                       choices = c("Agrar-, Forst- und Ernährungswissenschaften, Veterinärmedizin",
+                                   "Biologie",
+                                   "Geowissenschaften und Geographie",
+                                   "Informatik",
+                                   "Maschinenbau/Verfahrenstechnik",
+                                   "Nicht MINT",
+                                   "MINT",
+                                   "Vermessungswesen",
+                                   "Architektur, Innenarchitektur",
+                                   "Bauingenieurwesen",
+                                   "Chemie",
+                                   "Mathematik",
+                                   "Materialwissenschaft und Werkstofftechnik",
+                                   "Humanmedizin/Gesundheitswissenschaften",
+                                   "Geisteswissenschaften",
+                                   "Ingenieurwissenschaften",
+                                   "Ingenieurwissenschaften ohne Informatik",
+                                   "Physik, Astronomie",
+                                   "Rechts-, Wirtschafts- und Sozialwissenschaften",
+                                   "Mathematik, Naturwissenschaften",
+                                   "Naturwissenschaften",
+                                   "Pharmazie",
+                                   "Raumplanung",
+                                   "Sport",
+                                   "Verkehrstechnik, Nautik",
+                                   "Wirtschaftsingenieurwesen mit ingenieurwissenschaftlichem Schwerpunkt",
+                                   "Kunst, Kunstwissenschaft",
+                                   "Elektrotechnik und Informationstechnik"),
+
+                       selected = c(
+                         "Informatik", "MINT"),
+                       multiple = TRUE,
+                       options =  list(
+                         "max-options" = 2,
+                         "max-options-text" = "Maximal 2 Indikatoren auswählen")
+                     ))
   )
 
 }
@@ -90,6 +130,10 @@ mod_studium_studienzahl_bl_map_server <- function(id, r){
 
     observeEvent(input$map_l, {
       r$map_l <- input$map_l
+    })
+
+    observeEvent(input$map_f_lehr, {
+      r$map_f_lehr <- input$map_f_lehr
     })
 
     observeEvent(input$map_f, {
