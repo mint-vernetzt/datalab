c5c73475fc68c06a83758218dc53d8f8b1186c71
Abbey <Kekeli.Abbey@stifterverband.de>
2023-11-20 14:26:03 +0100

    löschung überflüssiger ui-funktionen

--- R/mod_beruf.R
+++ R/mod_beruf.R
@@ -29,7 +29,7 @@ mod_beruf_server <- function(id, r){
   mod_beruf_arbeitsmarkt_server("mod_beruf_arbeitsmarkt_ui_1", r)
 
   # Box 2
-  mod_beruf_arbeitsmarkt_einstieg_server("mod_beruf_arbeitsmarkt_einstieg_ui_1", r)
+  #mod_beruf_arbeitsmarkt_einstieg_server("mod_beruf_arbeitsmarkt_einstieg_ui_1", r)
   mod_beruf_arbeitsmarkt_einstieg_verlauf_server("mod_beruf_arbeitsmarkt_einstieg_verlauf_ui_1", r)
   mod_beruf_arbeitsmarkt_einstieg_vergleich_server("mod_beruf_arbeitsmarkt_einstieg_vergleich_ui_1", r)
 
@@ -40,14 +40,14 @@ mod_beruf_server <- function(id, r){
 
   # Box 4
   mod_beruf_arbeitsmarkt_anforderungen_server("mod_beruf_arbeitsmarkt_anforderungen_ui_1", r)
-  mod_beruf_arbeitsmarkt_anforderungen_verlauf_server("mod_beruf_arbeitsmarkt_anforderungen_verlauf_ui_1", r)
-  mod_beruf_arbeitsmarkt_anforderungen_vergleich_server("mod_beruf_arbeitsmarkt_anforderungen_vergleich_ui_1", r)
+  #mod_beruf_arbeitsmarkt_anforderungen_verlauf_server("mod_beruf_arbeitsmarkt_anforderungen_verlauf_ui_1", r)
+  #mod_beruf_arbeitsmarkt_anforderungen_vergleich_server("mod_beruf_arbeitsmarkt_anforderungen_vergleich_ui_1", r)
   mod_beruf_arbeitsmarkt_überblick_fächer_server("mod_beruf_arbeitsmarkt_überblick_fächer_ui_1", r)
 
   # Box 5
   mod_beruf_arbeitsmarkt_anforderungen_gender_server("mod_beruf_arbeitsmarkt_anforderungen_gender_ui_1", r)
-  mod_beruf_arbeitsmarkt_anforderungen_gender_verlauf_server("mod_beruf_arbeitsmarkt_anforderungen_gender_verlauf_ui_1", r)
-  mod_beruf_arbeitsmarkt_anforderungen_gender_vergleich_server("mod_beruf_arbeitsmarkt_anforderungen_gender_vergleich_ui_1", r)
+  #mod_beruf_arbeitsmarkt_anforderungen_gender_verlauf_server("mod_beruf_arbeitsmarkt_anforderungen_gender_verlauf_ui_1", r)
+  #mod_beruf_arbeitsmarkt_anforderungen_gender_vergleich_server("mod_beruf_arbeitsmarkt_anforderungen_gender_vergleich_ui_1", r)
 
   # Box 6
   mod_beruf_arbeitsmarkt_bl_server("mod_beruf_arbeitsmarkt_bl_ui_1", r)
@@ -58,17 +58,17 @@ mod_beruf_server <- function(id, r){
   # Box 7
   mod_beruf_arbeitsmarkt_bl_gender_server("mod_beruf_arbeitsmarkt_bl_gender_ui_1", r)
   mod_beruf_arbeitsmarkt_bl_gender_verlauf_server("mod_beruf_arbeitsmarkt_bl_gender_verlauf_ui_1", r)
-  mod_beruf_arbeitsmarkt_bl_gender_vergleich_server("beruf_arbeitsmarkt_bl_gender_vergleich_ui_1", r)
+  #mod_beruf_arbeitsmarkt_bl_gender_vergleich_server("beruf_arbeitsmarkt_bl_gender_vergleich_ui_1", r)
 
   # Box 8
   mod_beruf_arbeitsmarkt_landkreis_map_server("mod_beruf_arbeitsmarkt_landkreis_map_ui_1", r)
   mod_beruf_arbeitsmarkt_landkreis_vergleich_server("mod_beruf_arbeitsmarkt_landkreis_vergleich_ui_1", r)
-  mod_beruf_arbeitsmarkt_landkreis_table_lk_server("mod_beruf_arbeitsmarkt_landkreis_table_lk_ui_1", r )
-  mod_beruf_arbeitsmarkt_landkreis_table_lk_server("mod_beruf_arbeitsmarkt_landkreis_table_lk_ui_2", r )
-  mod_beruf_arbeitsmarkt_landkreis_table_lk_server("mod_beruf_arbeitsmarkt_landkreis_table_lk_ui_3", r)
+  #mod_beruf_arbeitsmarkt_landkreis_table_lk_server("mod_beruf_arbeitsmarkt_landkreis_table_lk_ui_1", r )
+  #mod_beruf_arbeitsmarkt_landkreis_table_lk_server("mod_beruf_arbeitsmarkt_landkreis_table_lk_ui_2", r )
+  #mod_beruf_arbeitsmarkt_landkreis_table_lk_server("mod_beruf_arbeitsmarkt_landkreis_table_lk_ui_3", r)
 
   # was ist das?
-  mod_beruf_arbeitsmarkt_landkreis_table_lk_analysis_server("xxxxxxxxxxx", btn, r)
+  #mod_beruf_arbeitsmarkt_landkreis_table_lk_analysis_server("xxxxxxxxxxx", btn, r)
 
 
 }
