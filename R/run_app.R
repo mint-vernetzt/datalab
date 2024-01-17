3020ab930369ff536dd5c1b3f631e98214ce887e
Brunner <Katharina.Brunner@stifterverband.de>
2024-01-17 18:32:22 +0100

    con in main einkommentiert

--- R/run_app.R
+++ R/run_app.R
@@ -11,7 +11,7 @@ run_app <- function(
   onStart =  function() {
     # Sys.setlocale(category = "LC_ALL", locale = "German_Germany.utf8")
     #
-    # con <<- DBI::dbConnect(RSQLite::SQLite(), "data/mint_db.sqlite", encoding = "UTF-8")
+     con <<- DBI::dbConnect(RSQLite::SQLite(), "data/mint_db.sqlite", encoding = "UTF-8")
   },
   options = list(),
   enableBookmarking = NULL,
