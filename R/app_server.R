9ade59f9362752e293f3ab42fe82185a8d9bb8d8
Brunner <Katharina.Brunner@stifterverband.de>
2024-02-06 09:29:26 +0100

    optionale lösung - lazy loading der unterseiten

--- R/app_server.R
+++ R/app_server.R
@@ -6,8 +6,8 @@
 #' @noRd
 app_server <- function(input, output, session) {
   # Your application server logic
- # con <<- DBI::dbConnect(RSQLite::SQLite(), "data/mint_db.sqlite", encoding = "UTF-8")
 
+  # Alte Logik
 
   # callModule(mod_home_server, "home_ui_1", data=example_data)
   mod_home_server("home_ui_1")
@@ -18,6 +18,47 @@ app_server <- function(input, output, session) {
   mod_studium_server("studium_ui_1")
 
   mod_beruf_server("beruf_ui_1")
+
+  # Lazy Loading Logik
+
+  # shinyjs::enable("tabs")  # Dies ermöglicht das dynamische Umschalten von Registerkarten mit shinyjs
+  #
+  # observe({
+  #   # Hier können Sie bedingte Logik basierend auf der ausgewählten Registerkarte implementieren
+  #   if (input$tabs == "home") {
+  #
+  #     mod_home_server("home_ui_1")
+  #     shinyjs::disable("schule")  # Hier können Sie andere Registerkarten deaktivieren, wenn nötig
+  #     shinyjs::disable("studium")
+  #     shinyjs::disable("beruf")
+  #
+  #   } else if (input$tabs == "schule") {
+  #
+  #     mod_schule_server("schule_ui_1")
+  #     shinyjs::disable("home")
+  #     shinyjs::disable("studium")
+  #     shinyjs::disable("beruf")
+  #
+  #   } else if (input$tabs == "studium"){
+  #
+  #     mod_studium_server("studium_ui_1")
+  #     shinyjs::disable("home")
+  #     shinyjs::disable("schule")
+  #     shinyjs::disable("beruf")
+  #
+  #   } else if (input$tabs == "beruf"){
+  #
+  #     mod_beruf_server("beruf_ui_1")
+  #     shinyjs::disable("home")
+  #     shinyjs::disable("schule")
+  #     shinyjs::disable("studium")
+  #   }
+  # })
+  #
+  # shinyjs::runjs("$('#tabs a[href=\"#home\"]').tab('show');")
+
+
+  # Disconnect funktioniert nicht wenn implementiert bei publishen??
 
   # session$onSessionEnded(function() {
   #   if (!is.null(con) && DBI::dbIsValid(con)) {
