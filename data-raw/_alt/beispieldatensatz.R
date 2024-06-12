50db555516c581ba40e8745ac0f5d761c4045392
Brunner <Katharina.Brunner@stifterverband.de>
2024-06-12 13:21:52 +0200

    alten data_raw odner entfernt

--- data-raw/_alt/beispieldatensatz.R
+++
@@ -1,6 +0,0 @@
-## code to prepare `DATASET` dataset goes here
-
-example_data <- readxl::read_xlsx(system.file(package="datalab", "data-raw/beispieldatensatz.xlsx"), sheet = 1) %>%
-  janitor::clean_names()
-
-usethis::use_data(example_data)
