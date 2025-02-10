e3c2c15cc31b722622222d3829aa0081ab0f97e0
Brunner <Katharina.Brunner@stifterverband.de>
2025-02-10 11:54:53 +0100

    Farb-Fix Studi-Pies und Veröffentichen

--- R/fct_plots_studium.R
+++ R/fct_plots_studium.R
@@ -65,7 +65,7 @@ studienzahl_mint <- function(r){
 
           highcharter::hw_grid(
 
-            out <- piebuilder(df_pie, titel, x = "fach", y = "proportion", tooltip, color =  c("#b16fab", "#efe8e6"), format = format),
+            out <- piebuilder(df_pie, titel, x = "fach", y = "proportion", tooltip, color =  c("#efe8e6","#b16fab"), format = format),
 
             ncol = 1,
             browsable = TRUE)
