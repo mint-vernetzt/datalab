b2e4065aedf3fa30c3f7d879a7fdd1f8d3decf69
Isfort <Marietta.Isfort@stifterverband.de>
2026-08-25 10:32:44 +0200

    kurzanalysen kacheln neu

--- R/mod_startseite_ui.R
+++ R/mod_startseite_ui.R
@@ -62,25 +62,175 @@ mod_startseite_start_ui <- function(id){
           p(),
 
     # Kurzanalysen ----
-            h2("Was steckt hinter den MINT-Statistiken?"),
+            h2("MINT-Themen im Fokus: Aktuelle Entwicklungen im Blick"),
             p(
-              "Ausgewählte Statistiken bereiten wir in Kurzanalysen auf.
-              Hier schauen wir hinter die Zahlen und ordnen sie ein – erklärend,
-              lösungsorientiert und wissenschaftlich fundiert.
-           Die Kurzanalysen können auf den verschiedenen Bereichs- und Themenseiten
-              im MINT-DataLab heruntergeladen werden.", br(),
-              "Auf der MINTvernetzt-Website, auf der Themenseite des MINT-DataLabs,
-              finden sich alle Kurzanalsyen auf einen Blick.",
+              "Unsere Kurzanalysen ordnen ausgewählte MINT-Kennzahlen ein,
+              greifen aktuelle Entwicklungen auf und zeigen mögliche Handlungsansätze.
+              Die Analysen basieren auf Daten aus dem MINT-DataLab und wissenschaftlichen Erkenntnissen.",
               br()),
-              tags$a(href = "https://www.mint-vernetzt.de/mint-datalab/#kurzanalysen",
-                     target = "_blank", "Alle Kurzanalysen",
+              tags$a(href = "https://www.mint-vernetzt.de/blogbeitraege/",
+                     target = "_blank", "Weitere Schwerpunktthemen von MINTvernetzt",
                      class = "btn btn-default",
                      style = "margin-bottom: 30px; margin-top: 10px; font-size: 18px")
     ),
-
     column(
-      width = 11,
-      slickR::slickROutput(ns("slider_output"), width = '800px', height = '500px'),
+      width = 12,
+
+      tags$div(
+
+        style = "
+      display:flex;
+      overflow-x:auto;
+      gap:24px;
+      padding:10px 0;
+      align-items:stretch;
+        ",
+
+        # Kachel 1
+        tags$div(
+          style = "min-width:400px; max-width:400px; background:white;border-radius:12px;padding:20px;display:flex;flex-direction:column;flex-shrink:0;",
+          tags$img(src="www/Visual KA schulisch-außerschulisch.png",
+                   loading = "lazy",
+                   decoding = "async",
+                   style="width:100%;height:220px;object-fit:cover;"),
+          tags$h3("MINT-Kooperationen machen Schule"),
+          tags$p("Kooperationen zwischen Schulen und außerschulischen Bildungsakteur:innen sind zentral für eine zukunftsfähige MINT-Bildung, nicht zuletzt im Kontext des Ganztags. Die Ergebnisse verschiedener MINTvernetzt-Befragungen zeigen auf einen Blick, welche Voraussetzungen und Bedarfe Schulen und außerschulische Partner für eine gelingende Zusammenarbeit sehen.",
+                 style = "flex-grow:1;"),
+          tags$a(href="https://www.mint-vernetzt.de/content/uploads/2026/06/MINTvernetzt_Kurzanalyse_schulisch_ausserschulisch.pdf",
+                 class="btn btn-primary",
+                 target = "_blank",
+                 style = "background-color: #b16fab;border:1px solid #b16fab;
+                 margin-top:auto; width:fit-content;",
+                 "Download Kurzanalyse")
+        ),
+
+        # Kachel 2
+        tags$div(
+          style = "min-width:400px; max-width:400px; background:white;border-radius:12px;padding:20px;display:flex;flex-direction:column;flex-shrink:0;",
+          tags$img(src="www/M I N T Fachkräftemangel.jpg",
+                   loading = "lazy",
+                   decoding = "async",
+                   style="width:100%;height:220px;object-fit:cover;"),
+          tags$h3("Fachkräftemangel in MINT: Eine Frage der Disziplin?"),
+          tags$p("Informatik boomt, aber es gibt immer weniger junge Leute, die sich für Technik und Naturwissenschaften interessieren. Gleichzeitig fehlen in der Technik besonders viele Fachkräfte. Hier schauen wir uns den Fachkräftemangel in den MINT-Bereichen genauer an, und geben Ideen, wie gezielt gegengesteuert werden kann.",
+                 style = "flex-grow:1;"),
+          tags$a(href="https://www.mint-vernetzt.de/content/uploads/2026/01/MINTvernetzt_Kurzanalyse_Fachkraeftemangel_MINT-Disziplinen.pdf",
+                 class="btn btn-primary",
+                 target = "_blank",
+                 style = "background-color: #b16fab;border:1px solid #b16fab;
+                 margin-top:auto; width:fit-content;",
+                 "Download Kurzanalyse")
+        ),
+
+        # Kachel 3
+        tags$div(
+          style = "min-width:400px; max-width:400px; background:white;border-radius:12px;padding:20px;display:flex;flex-direction:column;flex-shrink:0;",
+          tags$img(src="www/Zuwanderung_MINT.jpg",
+                   loading = "lazy",
+                   decoding = "async",
+                   style="width:100%;height:220px;object-fit:cover;"),
+          tags$h3("Szenarien: Zuwanderung für MINT"),
+          tags$p("Die MINT-Branche wird immer internationaler. In dieser Kurzanalyse werfen wir einen gezielten Blick darauf, welche Rolle Zuwanderung in den kommenden 15 Jahren für die Zahl der MINT-Fachkräfte spielen könnte. Die möglichen Szenarien hat für uns das IW Köln berechnet.",
+                 style = "flex-grow:1;"),
+          tags$a(href="https://www.mint-vernetzt.de/content/uploads/2026/01/MINTvernetzt_Kurzanalyse_Zukunftsszenarien_Zuwanderung_MINT.pdf",
+                 class="btn btn-primary",
+                 target = "_blank",
+                 style = "background-color: #b16fab;border:1px solid #b16fab;
+                 margin-top:auto; width:fit-content;",
+                 "Download Kurzanalyse")
+        ),
+
+        # Kachel 4
+        tags$div(
+          style = "min-width:400px; max-width:400px; background:white;border-radius:12px;padding:20px;display:flex;flex-direction:column;flex-shrink:0;",
+          tags$img(src="www/Zukunftsszenarien.jpg",
+                   loading = "lazy",
+                   decoding = "async",
+                   style="width:100%;height:220px;object-fit:cover;"),
+          tags$h3("Szenarien: So könnte sich die Zahl der MINT-Fachkräfte entwickeln"),
+          tags$p("Projektion bis 2037: Wird es 1,4 Mio. mehr oder 1,1 Mio. weniger MINT-Fachkräfte geben als heute? Das IW Köln hat für uns Szenarien berechnet und wir fassen hier zusammen, wie sehr sich MINT-Bildung, Frauenförderung, der Verbleib älterer Fachkräfte und Zuwanderung auf die MINT-Fachkräftezahlen auswirken können.",
+                 style = "flex-grow:1;"),
+          tags$a(href="https://www.mint-vernetzt.de/content/uploads/2026/01/MINTvernetzt_Kurzanalyse_Zukunftsszenarien_MINT-Fachkraefte.pdf",
+                 class="btn btn-primary",
+                 target = "_blank",
+                 style = "background-color: #b16fab;border:1px solid #b16fab;
+                 margin-top:auto; width:fit-content;",
+                 "Download Kurzanalyse")
+        ),
+
+        # Kachel 5
+        tags$div(
+          style = "min-width:400px; max-width:400px; background:white;border-radius:12px;padding:20px;display:flex;flex-direction:column;flex-shrink:0;",
+          tags$img(src="www/Mädchen_MINT.jpg",
+                   loading = "lazy",
+                   decoding = "async",
+                   style="width:100%;height:220px;object-fit:cover;"),
+          tags$h3("Mathe-Förderung für Mädchen: Notwendig und möglich"),
+          tags$p("Die aktuellen IQB-Bildungstrend-Ergebnisse zeigen: Mädchen schneiden im Mathematik-Kompetenztest erneut weniger gut ab als Jungen. Mögliche Ursachen könnten ein geringeres Interesse und Vertrauen in die eigenen Fähigkeiten in diesem Fach sein. Hier gehen wir darauf ein, wie Mathe-Förderung für Mädchen und Frauen gelingt.",
+                 style = "flex-grow:1;"),
+          tags$a(href="https://www.mint-vernetzt.de/content/uploads/2026/01/MINTvernetzt_Kurzanalyse_Maedchen_Mathematik.pdf",
+                 class="btn btn-primary",
+                 target = "_blank",
+                 style = "background-color: #b16fab;border:1px solid #b16fab;
+                 margin-top:auto; width:fit-content;",
+                 "Download Kurzanalyse")
+        ),
+
+        # Kachel 6
+        tags$div(
+          style = "min-width:400px; max-width:400px; background:white;border-radius:12px;padding:20px;display:flex;flex-direction:column;flex-shrink:0;",
+          tags$img(src="www/Chancengerechtigkeit.jpg",
+                   loading = "lazy",
+                   decoding = "async",
+                   style="width:100%;height:220px;object-fit:cover;"),
+          tags$h3("Mit MINT-Bildung zu mehr Chancengerechtigkeit"),
+          tags$p("Die soziale Herkunft von Kindern beeinflusst deren Chancen entlang ihres Bildungswegs. Das gilt auch für den Erwerb von MINT-Kompetenzen. Wir erklären die Zusammenhänge und zeigen, welche Schritte für mehr Chancengerechtigkeit in der (MINT-)Bildung sorgen.",
+                 style = "flex-grow:1;"),
+          tags$a(href="https://www.mint-vernetzt.de/content/uploads/2026/01/MINTvernetzt_Kurzanalyse_Chancengerechtigkeit_mit_MINT.pdf",
+                 class="btn btn-primary",
+                 target = "_blank",
+                 style = "background-color: #b16fab;border:1px solid #b16fab;
+                 margin-top:auto; width:fit-content;",
+                 "Download Kurzanalyse")
+        ),
+
+        # Kachel 7
+        tags$div(
+          style = "min-width:400px; max-width:400px; background:white;border-radius:12px;padding:20px;display:flex;flex-direction:column;flex-shrink:0;",
+          tags$img(src="www/Frauen_verlassen_MINT.jpg",
+                   loading = "lazy",
+                   decoding = "async",
+                   style="width:100%;height:220px;object-fit:cover;"),
+          tags$h3("Frauen verlassen MINT - vier Lösungsansätze"),
+          tags$p("Frauen entscheiden sich seltener für eine MINT-Ausbildung oder einen MINT-Beruf. Während in MINT-Leistungskursen das Geschlechterverhältnis nahezu ausgeglichen ist, nimmt der Frauenanteil über die Ausbildung bis zum Beruf ab. Wir zeigen Ansätze aus der Wissenschaft, wie wir Frauen in MINT stärken.",
+                 style = "flex-grow:1;"),
+          tags$a(href="https://www.mint-vernetzt.de/content/uploads/2026/01/MINTvernetzt_Kurzanalyse_Frauen_in_MINT-Berufen.pdf",
+                 class="btn btn-primary",
+                 target = "_blank",
+                 style = "background-color: #b16fab;border:1px solid #b16fab;
+                 margin-top:auto; width:fit-content;",
+                 "Download Kurzanalyse")
+        ),
+
+        # Kachel 8
+        tags$div(
+          style = "min-width:400px; max-width:400px; background:white;border-radius:12px;padding:20px;display:flex;flex-direction:column;flex-shrink:0;",
+          tags$img(src="www/Fachkräfte_von_morgen.jpg",
+                   loading = "lazy",
+                   decoding = "async",
+                   style="width:100%;height:220px;object-fit:cover;"),
+          tags$h3("Wo die MINT-Fachkräfte von morgen herkommen"),
+          tags$p("Während der Bedarf an MINT-Fachkräften stetig wächst, sinkt die Zahl an MINT-Auszubildenden und MINT-Studienanfänger:innen seit einigen Jahren. Wir gehen auf diese Entwicklung ein und tragen Anregungen von Bildungsexpert:innen zusammen, die diesem Trend entgegenwirken sollen.",
+                 style = "flex-grow:1;"),
+          tags$a(href="https://www.mint-vernetzt.de/content/uploads/2026/01/MINTvernetzt_Kurzanalyse_Interesse_an_MINT.pdf",
+                 class="btn btn-primary",
+                 target = "_blank",
+                 style = "background-color: #b16fab;border:1px solid #b16fab;
+                 margin-top:auto; width:fit-content;",
+                 "Download Kurzanalyse")
+        )
+      ),
+
 
       # Lernvideo ----
 
@@ -133,16 +283,7 @@ mod_startseite_start_ui <- function(id){
 #' @noRd
 mod_startseite_start_server <- function(id){
   moduleServer( id, function(input, output, session){
-    ns <- session$ns
 
-    output$slider_output <- slickR::renderSlickR({
-
-      slickR::slickR(
-        obj =  paste0("inst/app/www/slide/", grep("_slide", list.files("inst/app/www/slide"), value = TRUE)),
-        height = 450,
-        width = "95%"
-      )
-    })
 
   })
 }
